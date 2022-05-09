da0f0d50e103302f1d3aaa650830098f9343d554
JohnSak <giannossak@gmail.com>
2022-05-09 22:54:12 +0300

    starting

--- main/symbolTable.java
+++ main/symbolTable.java
@@ -1,2 +1,6 @@
+import java.util.Map;
+import java.util.Hashtable;
+
 class symbolTable<Key extends Comparable<Key>, Value> {
+    Map<String, String> ST = new Hashtable<>();
 }
--- main/symbolTableVisitor.java
+++ main/symbolTableVisitor.java
@@ -1,6 +1,44 @@
 import syntaxtree.*;
 import visitor.*;
 
+import java.util.Map;
+import java.util.HashMap;
+
+class symType {
+    String name, type;
+}
+
 class symbolTableVisitor extends GJDepthFirst<String, String> {
-    public symbolTable<String, String> ST;
+    Map<String, Map<String, symType>> ST = new HashMap<String, Map<String, symType>>();
+
+    /**
+    * f0 -> "class"
+    * f1 -> Identifier()
+    * f2 -> "{"
+    * f3 -> "public"
+    * f4 -> "static"
+    * f5 -> "void"
+    * f6 -> "main"
+    * f7 -> "("
+    * f8 -> "String"
+    * f9 -> "["
+    * f10 -> "]"
+    * f11 -> Identifier()
+    * f12 -> ")"
+    * f13 -> "{"
+    * f14 -> ( VarDeclaration() )*
+    * f15 -> ( Statement() )*
+    * f16 -> "}"
+    * f17 -> "}"
+    */
+    @Override
+    public String visit(MainClass n, String argu) throws Exception {
+        String className = n.f1.accept(this, "GLOBAL");
+        if (this.ST.containsKey(className)) throw new Exception();
+        else {
+            Map<String, symType> innerST = new HashMap<String, symType>();
+            this.ST.put("GLOAL", innerST);
+        }
+        return null;
+    }
 }
