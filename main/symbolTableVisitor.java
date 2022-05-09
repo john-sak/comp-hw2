f443ead83f12fd525108ea150638275234143479
JohnSak <giannossak@gmail.com>
2022-05-09 15:27:27 +0300

    created new my_visitor files

---
+++ main/symbolTableVisitor.java
@@ -0,0 +1,6 @@
+import syntaxtree.*;
+import visitor.*;
+
+class symbolTableVisitor extends GJDepthFirst<String, String> {
+    //todo
+}
---
+++ main/typeCheckVisitor.java
@@ -0,0 +1,6 @@
+import syntaxtree.*;
+import visitor.*;
+
+class typeCheckVisitor extends GJDepthFirst<String, String> {
+    //todo
+}
