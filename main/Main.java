770866607d0339c6e1789d174b37944ce3d35bac
JohnSak <giannossak@gmail.com>
2022-05-13 22:12:25 +0300

    it works for pt 2?

--- main/Main.java
+++ main/Main.java
@@ -4,8 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         if(args.length < 1){
@@ -22,22 +20,22 @@ public class Main {
                 symbolTableVisitor STVisitor = new symbolTableVisitor();
                 root.accept(STVisitor, null);
                 System.out.println("Program in inputFile \"" + args[i] + "\" idk.");
-                System.out.println(STVisitor.globalST);
-                System.out.println(STVisitor.globalST.get("Example"));
-                System.out.println(STVisitor.globalST.get("Example").fields);
-                System.out.println(STVisitor.globalST.get("Example").methods);
-                System.out.println(STVisitor.globalST.get("A"));
-                System.out.println(STVisitor.globalST.get("A").fields);
-                System.out.println(STVisitor.globalST.get("A").methods);
-                System.out.println(STVisitor.globalST.get("B"));
-                System.out.println(STVisitor.globalST.get("B").fields);
-                System.out.println(STVisitor.globalST.get("B").methods);
-                // TCArgs argu = new TCArgs();
-                // argu.globalST = STVisitor.globalST;
-                // typeCheckVisitor TCVisitor = new typeCheckVisitor();
-                // root.accept(TCVisitor, argu);
-                // System.out.println("Program in inputFile \"" + args[i] + "\" idk.");
-                // System.out.println("todo print rest");
+                // System.out.println(STVisitor.globalST);
+                // System.out.println(STVisitor.globalST.get("Example"));
+                // System.out.println(STVisitor.globalST.get("Example").fields);
+                // System.out.println(STVisitor.globalST.get("Example").methods);
+                // System.out.println(STVisitor.globalST.get("A"));
+                // System.out.println(STVisitor.globalST.get("A").fields);
+                // System.out.println(STVisitor.globalST.get("A").methods);
+                // System.out.println(STVisitor.globalST.get("B"));
+                // System.out.println(STVisitor.globalST.get("B").fields);
+                // System.out.println(STVisitor.globalST.get("B").methods);
+                TCArgs argu = new TCArgs();
+                argu.globalST = STVisitor.globalST;
+                typeCheckVisitor TCVisitor = new typeCheckVisitor();
+                root.accept(TCVisitor, argu);
+                System.out.println("Program in inputFile \"" + args[i] + "\" idk.");
+                System.out.println("todo print rest");
             }
             catch(ParseException ex){
                 System.out.println(ex.getMessage() + " inputFile \"" + args[i] + "\"");
