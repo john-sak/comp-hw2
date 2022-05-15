901bf7a3636dae9d4de99efed8bf1bc3adbbf0f3
JohnSak <giannossak@gmail.com>
2022-05-15 21:03:03 +0300

    is it ok?

--- main/symbolTableVisitor.java
+++ main/symbolTableVisitor.java
@@ -1,20 +1,28 @@
 import syntaxtree.*;
 import visitor.*;
 
-import java.util.List;
-import java.util.ArrayList;
+// import java.util.List;
+// import java.util.ArrayList;
 
 import java.util.Map;
 import java.util.HashMap;
 
 class classInfo {
-    public List<Map<String, fieldInfo>> fields = new ArrayList<Map<String, fieldInfo>>();
-    public List<Map<String, methodInfo>> methods = new ArrayList<Map<String, methodInfo>>();
+    // public List<Map<String, fieldInfo>> fields = new ArrayList<Map<String, fieldInfo>>();
+    // public List<Map<String, methodInfo>> methods = new ArrayList<Map<String, methodInfo>>();
+    public String name;
+    public Map<String, fieldInfo> fields = new HashMap<String, fieldInfo>();
+    public Map<String, methodInfo> methods = new HashMap<String, methodInfo>();
+    public classInfo superclass = null;
 
-    classInfo() {
-        this.fields.add(new HashMap<String, fieldInfo>());
-        this.methods.add(new HashMap<String, methodInfo>());
+    classInfo(String name) {
+        this.name = name;
     }
+
+    // classInfo() {
+    //     this.fields.add(new HashMap<String, fieldInfo>());
+    //     this.methods.add(new HashMap<String, methodInfo>());
+    // }
 }
 
 class fieldInfo {
@@ -57,9 +65,11 @@ class symbolTableVisitor extends GJDepthFirst<String, String> {
     @Override
     public String visit(MainClass n, String argu) throws Exception {
         String className = n.f1.accept(this, null);
-        if (this.globalST.put(className, new classInfo()) != null) throw new Exception();
-        if (this.globalST.get(className).methods.get(0).put("main", new methodInfo()) != null) throw new Exception();
-        methodInfo methodI = this.globalST.get(className).methods.get(0).get("main");
+        if (this.globalST.put(className, new classInfo(className)) != null) throw new Exception();
+        if (this.globalST.get(className).methods.put("main", new methodInfo()) != null) throw new Exception();
+        // if (this.globalST.get(className).methods.get(0).put("main", new methodInfo()) != null) throw new Exception();
+        methodInfo methodI = this.globalST.get(className).methods.get("main");
+        // methodInfo methodI = this.globalST.get(className).methods.get(0).get("main");
         methodI.returnValue = "void";
         methodI.argNum = 1;
         methodI.argTypes = "String[]";
@@ -79,7 +89,7 @@ class symbolTableVisitor extends GJDepthFirst<String, String> {
     @Override
     public String visit(ClassDeclaration n, String argu) throws Exception {
         String className = n.f1.accept(this, null);
-        if (this.globalST.put(className, new classInfo()) != null) throw new Exception();
+        if (this.globalST.put(className, new classInfo(className)) != null) throw new Exception();
         n.f3.accept(this, className);
         n.f4.accept(this, className);
         return null;
@@ -99,23 +109,37 @@ class symbolTableVisitor extends GJDepthFirst<String, String> {
     public String visit(ClassExtendsDeclaration n, String argu) throws Exception {
         String className = n.f1.accept(this, null), classExtends = n.f3.accept(this, null);
         if (!this.globalST.containsKey(classExtends)) throw new Exception();
-        if (this.globalST.put(className, new classInfo()) != null) throw new Exception();
+        if (this.globalST.put(className, new classInfo(className)) != null) throw new Exception();
         n.f5.accept(this, className);
         n.f6.accept(this, className);
         classInfo thisClass = this.globalST.get(className), superClass = this.globalST.get(classExtends);
-        thisClass.fields.addAll(superClass.fields);
-        thisClass.methods.addAll(superClass.methods);
-        Map<String, methodInfo> thisClassMethods = thisClass.methods.get(0);
-        for (int i = 1; i < thisClass.methods.size(); i++) {
-            Map<String, methodInfo> superClassMethods = thisClass.methods.get(i);
-            for (Map.Entry<String, methodInfo> entry : thisClassMethods.entrySet()) {
-                methodInfo infoSuper;
-                if ((infoSuper = superClassMethods.get(entry.getKey())) != null) {
-                    methodInfo infoThis = entry.getValue();
-                    if (infoSuper.returnValue.compareTo(infoThis.returnValue) != 0 || infoSuper.argNum != infoThis.argNum || infoSuper.argTypes.compareTo(infoThis.argTypes) != 0) throw new Exception();
+        // thisClass.fields.addAll(superClass.fields);
+        // thisClass.methods.addAll(superClass.methods);
+        // thisClass.superclasses.add(superClass);
+        // thisClass.superclasses.addAll(superClass.superclasses);
+        thisClass.superclass = superClass;
+        Map<String, methodInfo> newMethods = thisClass.methods;
+        while ((superClass = thisClass.superclass) != null) {
+            for (Map.Entry<String, methodInfo> currMethod : newMethods.entrySet()) {
+                methodInfo method2;
+                if ((method2 = superClass.methods.get(currMethod.getKey())) != null) {
+                    methodInfo method1 = currMethod.getValue();
+                    if (method1.returnValue.compareTo(method2.returnValue) != 0 || method1.argNum != method2.argNum || method1.argTypes.compareTo(method2.argTypes) != 0) throw new Exception();
                 }
             }
+            thisClass = superClass;
         }
+        // Map<String, methodInfo> thisClassMethods = thisClass.methods;
+        // for (int i = 1; i < thisClass.methods.size(); i++) {
+        //     Map<String, methodInfo> superClassMethods = thisClass.methods.get(i);
+        //     for (Map.Entry<String, methodInfo> entry : thisClassMethods.entrySet()) {
+        //         methodInfo infoSuper;
+        //         if ((infoSuper = superClassMethods.get(entry.getKey())) != null) {
+        //             methodInfo infoThis = entry.getValue();
+        //             if (infoSuper.returnValue.compareTo(infoThis.returnValue) != 0 || infoSuper.argNum != infoThis.argNum || infoSuper.argTypes.compareTo(infoThis.argTypes) != 0) throw new Exception();
+        //         }
+        //     }
+        // }
         return null;
     }
 
@@ -131,9 +155,9 @@ class symbolTableVisitor extends GJDepthFirst<String, String> {
         if ((classI = this.globalST.get(scopes[0])) == null) throw new Exception();
         if (argu.contains("->")) {
             methodInfo methodI;
-            if ((methodI = classI.methods.get(0).get(scopes[1])) == null) throw new Exception();
+            if ((methodI = classI.methods.get(scopes[1])) == null) throw new Exception();
             if (methodI.localVars.put(n.f1.accept(this, null), new fieldInfo(n.f0.accept(this, null))) != null) throw new Exception();
-        } else if (classI.fields.get(0).put(n.f1.accept(this, null), new fieldInfo(n.f0.accept(this, null))) != null) throw new Exception();
+        } else if (classI.fields.put(n.f1.accept(this, null), new fieldInfo(n.f0.accept(this, null))) != null) throw new Exception();
         return null;
     }
 
@@ -158,8 +182,8 @@ class symbolTableVisitor extends GJDepthFirst<String, String> {
         String methodName = n.f2.accept(this, null);
         classInfo classI;
         if ((classI = this.globalST.get(argu)) == null) throw new Exception();
-        if (classI.methods.get(0).put(methodName, new methodInfo()) != null) throw new Exception();
-        methodInfo methodI = classI.methods.get(0).get(methodName);
+        if (classI.methods.put(methodName, new methodInfo()) != null) throw new Exception();
+        methodInfo methodI = classI.methods.get(methodName);
         methodI.returnValue = n.f1.accept(this, null);
         String argList = (n.f4.present() ? n.f4.accept(this, null) : "");
         if (!argList.equals("")) {
--- main/typeCheckVisitor.java
+++ main/typeCheckVisitor.java
@@ -11,15 +11,24 @@ class TCArgs {
 class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
 
     public String resolveIdentifier(String ID, TCArgs argu) throws Exception {
-        if (!ID.contains("->")) throw new Exception();
-        String type;
+        if (!argu.scope.contains("->")) throw new Exception();
         String[] scope = argu.scope.split("->");
         classInfo classI;
         if ((classI = argu.globalST.get(scope[0])) == null) throw new Exception();
         methodInfo methodI;
-        if ((methodI = classI.methods.get(0).get(scope[1])) == null) throw new Exception();
-        // todo
-        return type;
+        if ((methodI = classI.methods.get(scope[1])) == null) throw new Exception();
+        fieldInfo fieldI;
+        if ((fieldI = methodI.localVars.get(ID)) != null) return fieldI.type;
+        // if ((fieldI = classI.fields.get(ID)) != null) return fieldI.type;
+        // while ((classI = classI.superclass) != null) if ((fieldI = classI.fields.get(ID)) != null) return fieldI.type;
+        while (classI != null) {
+            if ((fieldI = classI.fields.get(ID)) != null) return fieldI.type;
+            classI = classI.superclass;
+        }
+        // for (int i = 0; i < classI.superclasses.size(); i++) if (classI.superclasses.get(i).fields.get(0).containsKey(ID)) return classI.superclasses.get(i).fields.get(0).get(ID).type;
+        // if (classI.fields.contains(ID)) return classI.fields.get(0).get(ID).type;
+        // if (argu.globalST.containsKey(ID)) return ID;
+        return null;
     }
 
     public boolean isValidType(String type, TCArgs argu) {
@@ -27,7 +36,17 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
     }
 
     public boolean isAcceptable(String expected, String given, TCArgs argu) throws Exception {
-        return true;
+        if (!isValidType(given, argu)) return false;
+        String[] acceptable = expected.split("|");
+        for (String type : acceptable) {
+            if (!isValidType(type, argu)) continue;
+            if (given.compareTo(expected) == 0) return true;
+            classInfo classI = argu.globalST.get(given);
+            // if ((classI = argu.globalST.get(given)) == null) return false;
+            while ((classI = classI.superclass) != null) if (classI.name.compareTo(type) == 0) return true;
+            // if (classI.superclasses.contains(argu.globalST.get(expected))) return true;
+        }
+        return false;
     }
 
     /**
@@ -53,7 +72,7 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
     @Override
     public String visit(MainClass n, TCArgs argu) throws Exception {
         TCArgs oldArgu = argu;
-        argu.scope = n.f1.accept(this, null) + "->main";
+        argu.scope = n.f1.accept(this, argu) + "->main";
         n.f14.accept(this, argu);
         n.f15.accept(this, argu);
         argu = oldArgu;
@@ -71,7 +90,7 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
     @Override
     public String visit(ClassDeclaration n, TCArgs argu) throws Exception {
         TCArgs oldArgu = argu;
-        argu.scope = n.f1.accept(this, null);
+        argu.scope = n.f1.accept(this, argu);
         n.f3.accept(this, argu);
         n.f4.accept(this, argu);
         argu = oldArgu;
@@ -91,7 +110,7 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
     @Override
     public String visit(ClassExtendsDeclaration n, TCArgs argu) throws Exception {
         TCArgs oldArgu = argu;
-        argu.scope = n.f1.accept(this, null);
+        argu.scope = n.f1.accept(this, argu);
         n.f5.accept(this, argu);
         n.f6.accept(this, argu);
         argu = oldArgu;
@@ -105,8 +124,9 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
      */
     @Override
     public String visit(VarDeclaration n, TCArgs argu) throws Exception {
-        String type = n.f0.accept(this, null);
-        if (!isValidType(type, argu)) throw new Exception();
+        // String type = n.f0.accept(this, argu);
+        // if (!isValidType(type, argu)) throw new Exception();
+        if (!isValidType(n.f0.accept(this, argu), argu)) throw new Exception();
         return null;
     }
     
@@ -127,13 +147,14 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
      */
     @Override
     public String visit(MethodDeclaration n, TCArgs argu) throws Exception {
+        String type = n.f1.accept(this, argu);
+        if (!isValidType(type, argu)) throw new Exception();
         TCArgs oldArgu = argu;
-        argu.scope += "->" + n.f2.accept(this, null);
+        argu.scope += "->" + n.f2.accept(this, argu);
         n.f4.accept(this, argu);
         n.f7.accept(this, argu);
         n.f8.accept(this, argu);
-        String type = n.f1.accept(this, null);
-        if (!isValidType(type, argu) || !isAcceptable(type, n.f10.accept(this, null), argu)) throw new Exception();
+        if (!isAcceptable(type, n.f10.accept(this, argu), argu)) throw new Exception();
         argu = oldArgu;
         return null;
     }
@@ -144,7 +165,7 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
      */
     @Override
     public String visit(FormalParameter n, TCArgs argu) throws Exception {
-        if (!isValidType(n.f0.accept(this, null), argu)) throw new Exception();
+        if (!isValidType(n.f0.accept(this, argu), argu)) throw new Exception();
         return null;
     }
     
@@ -192,7 +213,7 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
      */
     @Override
     public String visit(AssignmentStatement n, TCArgs argu) throws Exception {
-        if (!isAcceptable(resolveIdentifier(n.f0.accept(this, null), argu), n.f2.accept(this, argu), argu)) throw new Exception();
+        if (!isAcceptable(resolveIdentifier(n.f0.accept(this, argu), argu), n.f2.accept(this, argu), argu)) throw new Exception();
         return null;
     }
     
@@ -207,11 +228,11 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
      */
     @Override
     public String visit(ArrayAssignmentStatement n, TCArgs argu) throws Exception {
-        String type = resolveIdentifier(n.f0.accept(this, null), argu);
+        String type = resolveIdentifier(n.f0.accept(this, argu), argu);
         if (!isAcceptable("boolean[]|int[]", type, argu)) throw new Exception();
         if (!isAcceptable("int", n.f2.accept(this, argu), argu)) throw new Exception();
-        type = type.substring(0, type.length() - 2);
-        if (!isAcceptable(type, n.f5.accept(this, argu), argu)) throw new Exception();
+        // type = type.substring(0, type.length() - 2);
+        if (!isAcceptable(type.substring(0, type.length() - 2), n.f5.accept(this, argu), argu)) throw new Exception();
         return null;
     }
     
@@ -361,16 +382,23 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
             if (!argu.scope.contains("->")) throw new Exception();
             scope = argu.scope.split("->")[0];
         }
-        String name = n.f2.accept(this, null);
+        String name = n.f2.accept(this, argu);
         classInfo classI;
         if ((classI = argu.globalST.get(scope)) == null) throw new Exception();
         methodInfo methodI = null;
-        for (Map<String, methodInfo> node : classI.methods) if ((methodI = node.get(name)) != null) break;
+        while (classI != null) {
+            if ((methodI = classI.methods.get(name)) != null) break;
+            classI = classI.superclass;
+        }
         if (methodI == null) throw new Exception();
-        String[] parameters = n.f4.accept(this, argu).split(", ");
-        if (parameters.length != methodI.argNum) throw new Exception();
-        String[] argumetns = methodI.argTypes.split(", ");
-        for (int i = 0; i < parameters.length; i++) if (parameters[i].compareTo(argumetns[i]) != 0) throw new Exception();
+        // for (Map<String, methodInfo> node : classI.methods) if ((methodI = node.get(name)) != null) break;
+        // if (methodI == null) throw new Exception();
+        if (n.f4.present()) {
+            String[] parameters = n.f4.accept(this, argu).split(", ");
+            if (parameters.length != methodI.argNum) throw new Exception();
+            String[] argumetns = methodI.argTypes.split(", ");
+            for (int i = 0; i < parameters.length; i++) if (parameters[i].compareTo(argumetns[i]) != 0) throw new Exception();
+        } else if (methodI.argNum != 0) throw new Exception();
         return methodI.returnValue;
     }
 
@@ -406,6 +434,7 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
     public String visit(PrimaryExpression n, TCArgs argu) throws Exception {
         String type = n.f0.accept(this, argu);
         if (!isValidType(type, argu)) type = resolveIdentifier(type, argu);
+        if (type == null) throw new Exception();
         return type;
     }
 
@@ -483,7 +512,9 @@ class typeCheckVisitor extends GJDepthFirst<String, TCArgs> {
      */
     @Override
     public String visit(AllocationExpression n, TCArgs argu) throws Exception {
-        String type = resolveIdentifier(n.f1.accept(this, null), argu);
+        String type = n.f1.accept(this, argu);
+        // if (!argu.globalST.containsKey(type)) throw new Exception();
+        if (!isValidType(type, argu)) throw new Exception();
         return type;
     }
 
